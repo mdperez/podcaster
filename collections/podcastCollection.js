149861dc69f92e335285fd87873f03cc5d606cef
Miguel Domínguez <miguel.dominguez@netexlearning.net>
2017-01-06 21:38:46 +0100

    colección de podcast guardada en localStorage y se repite la llamada hasta que haya pasado mas de un día

--- collections/podcastCollection.js
+++ collections/podcastCollection.js
@@ -2,10 +2,24 @@ PodcastCollection = Backbone.Collection.extend({
    url: 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json',
    model: PodcastModel,
    initialize: function(){
-      //comprobar si existe en localStorage, si no existe hacemos fetch y guardamos
-      this.fetch({async: false}).done(function(data){
-         //guardamos en localStorage
-      });
+      var self = this;
+      if ((localStorage.getItem('podcastCollection') !== null) && this.getDays()){
+         this.reset(JSON.parse(localStorage.getItem('podcastCollection')).data);
+      } else {
+         this.fetch({async: false}).done(function(data){
+            var localCollection = {
+               data: self.toJSON(),
+               date: new Date()
+            }
+            localStorage.setItem('podcastCollection', JSON.stringify(localCollection));
+         });
+      }
+   },
+   getDays: function() {
+      var now = new Date();
+      var date = Date.parse(JSON.parse(localStorage.getItem('podcastCollection')).date);
+      var days = moment.duration(now - date).asDays();
+      return (days < 1);
    },
    parse: function(data) {
       return data.feed.entry;
